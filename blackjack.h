7f53f953c11760f922a09553a62be105a197fbf9
Rashmi Korukonda <korukondarrao@gmail.com>
2025-07-02 12:16:27 -0500

    Added necessary structs for Card, Player, and Dealer

--- blackjack.h
+++ blackjack.h
@@ -7,4 +7,26 @@
 
 using namespace std;
 
+// Struct to represent a playing card
+struct Card 
+{
+    int value;         // Numeric value of the card
+    string name;       // Name of the card (e.g., "Ace", "2", "King")
+    string suit;       // Suit of the card (e.g., "Hearts", "Spades")
+};
+
+// Struct to represent a player
+struct Player 
+{
+    string name;               // Name of the player
+    double balance;            // Balance of the player
+    vector<Card> hand;         // Cards in the player's hand
+};
+
+// Struct to represent a dealer
+struct Dealer 
+{
+    vector<Card> hand;         // Cards in the dealer's hand
+};
+
 #endif
